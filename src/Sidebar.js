481f4a964e6af69a168ce399cdb7d0afdc7064d5
abhishek basnal <abbansal25@gmail.com>
2021-09-16 23:29:12 +0530

    all functioanlity added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,37 +1,37 @@
-import { React} from "react";
-import "./Sidebar.css"
-import TwitterIcon from '@material-ui/icons/Twitter';
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import SidebarOption from "./SidebarOption";
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from "@material-ui/icons/Search"
-import NotificationNoneIcon from "@material-ui/icons/NotificationsNone"
-import MailOutlineIcon from "@material-ui/icons/MailOutline"
-import BookmarkBorder from "@material-ui/icons/BookmarkBorder"
-import ListAlt from "@material-ui/icons/ListAlt"
-import PermIdentity from "@material-ui/icons/PermIdentity"
-import MoreHoriz from "@material-ui/icons/MoreHoriz"
-import {Button} from "@material-ui/core"
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
-function Sidebar(){
-    return(
-        <div className="sidebar">
-            {/* twitter icon */}
-            <TwitterIcon className="sidebar__twitterIcon"/>
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon className="sidebar__twitterIcon" />
 
-            <SidebarOption active Icon={HomeIcon} text = "home"/>
-            <SidebarOption Icon={SearchIcon} text="explore"/>
-            <SidebarOption Icon={NotificationNoneIcon} text= "notifications"/>
-            <SidebarOption Icon={MailOutlineIcon} text= "messages"/>
-            <SidebarOption Icon={BookmarkBorder} text= "bookmark"/>
-            <SidebarOption Icon={ListAlt} text= "lists"/>
-            <SidebarOption Icon={PermIdentity} text= "profile"/>
-            <SidebarOption Icon={MoreHoriz} text= "more"/>
-            
+      <SidebarOption active Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-            {/* twitterButton */}
-            <Button variant="outlined" className="sidebar__tweet" fullWidth>Tweet</Button>
-        </div>
-    )
+      {/* Button -> Tweet */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Widgets.css";
-// import {
-//   TwitterTimelineEmbed,
-//   TwitterShareButton,
-//   TwitterTweetEmbed,
-// } from "react-twitter-embed";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
@@ -18,18 +18,18 @@ function Widgets() {
       <div className="widgets__widgetContainer">
         <h2>What's happening</h2>
 
-        {/* <TwitterTweetEmbed tweetId={"858551177860055040"} />
+        {/* <TwitterTweetEmbed tweetId={"858551177860055040"} /> */}
 
         <TwitterTimelineEmbed
           sourceType="profile"
-          screenName="cleverqazi"
+          screenName="elonmusk"
           options={{ height: 400 }}
         />
 
         <TwitterShareButton
           url={"https://facebook.com/cleverprogrammer"}
           options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
-        /> */}
+        />
       </div>
     </div>
   );
